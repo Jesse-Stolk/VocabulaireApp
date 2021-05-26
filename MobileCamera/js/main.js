a6c89b50d1504117c96e9f1af519d67db189dafa
Boost9898 <bls.place@gmail.com>
2021-05-26 21:35:19 +0200

    Removed node(modules)

--- MobileCamera/js/main.js
+++ MobileCamera/js/main.js
@@ -12,14 +12,11 @@ console.log(`VERSIENUMMER: ${versionNumber}`)
 
 const translate = require('@vitalets/google-translate-api');
 
-translate('Ik spreek Engels', { to: 'en' }).then(res => {
-  console.log(res.text);
-  //=> I speak English
-  console.log(res.from.language.iso);
-  //=> nl
-}).catch(err => {
-  console.error(err);
-});
+translate('Ik spreek Engels', { to: 'en' })
+  .then(res => {
+    console.log(res.text); //=> I speak English
+    console.log(res.from.language.iso); //=> nl
+  });
 
 // function preload() {
 //   // Models available are 'cocossd', 'yolo'
